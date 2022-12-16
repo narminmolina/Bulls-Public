a59011f5f4b450e2c4b05eb07757148fb1e39736
Narmin Molina <narminmolina@gmail.com>
2022-12-15 22:13:07 -0500

    collection aside

--- src/pages/collection/index.js
+++ src/pages/collection/index.js
@@ -3,7 +3,21 @@ import { Base } from 'Base';
 const Collection = () => {
 	return (
 		<Base title="CollectionPage">
-			<p>Add everything for Collection page </p>
+			<div className="container">
+				<div className="collection">
+					<aside>
+						<h1>Collection</h1>
+						<p>Search the entire Bull Club Collections. Or connect your wallet and view your own collection</p>
+						<div className="tab-buttons">
+							<button>Main</button>
+							<button>Owned</button>
+						</div>
+					</aside>
+					<div className="card-wrapper">
+						<card></card>
+					</div>
+				</div>
+			</div>
 		</Base>
 	);
 };
