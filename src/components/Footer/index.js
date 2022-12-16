d383aadf83044d6acaba7484a00031a24d7e6450
Narmin Molina <narminmolina@gmail.com>
2022-12-15 21:45:01 -0500

    footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,34 @@
-// const navItems = [
-// 	{ title: 'Home', link: '/' },
-// 	{ title: 'Collection', link: '/collection' },
-// 	{ title: 'Recent Sales', link: '/recent-sales' },
-// 	{ title: 'The Bull Club', link: '/the-bull-club' },
-// ];
+import { Logo } from 'components/Logo';
+import { Link } from 'react-router-dom';
+import './index.scss';
+
+
+const socialMedias = [
+	{ title: 'Telegram', link: 'https://t.me/thebullclub' },
+	{ title: 'Twitter', link: 'https://twitter.com/TheBullClubNFT' },
+	{ title: 'Discord', link: 'https://discord.com/invite/thebullclub' },
+	{ title: 'Instagram', link: 'https://www.instagram.com/thebullclubnft/' },
+];
 
 export const Footer = () => {
-	return <footer>Hello foot</footer>;
+	return (
+		<footer>
+			<div className="container extended">
+				<div className="footer-wrapper">
+					<nav>
+						<ul>
+							{socialMedias.map(({ title, link }, index) => (
+								<li key={index}>
+									<a href={link} title={title} target="_blank" rel="noopener noreferrer">
+										{title}
+									</a>
+								</li>
+							))}
+						</ul>
+					</nav>
+					<Logo />
+				</div>
+			</div>
+		</footer>
+	);
 };
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -13,8 +13,8 @@ const navItems = [
 ];
 
 const marketplaces = [
-	{ label: 'MAGIC EDEN', value: 'magicEden', to: 'https://magiceden.io/marketplace/the_bull_club' },
-	{ label: 'OPENSEA', value: 'opensea', to: 'https://opensea.io/collection/the-bull-club-og' },
+	{ label: 'MAGIC EDEN', value: 'magicEden', link: 'https://magiceden.io/marketplace/the_bull_club' },
+	{ label: 'OPENSEA', value: 'opensea', link: 'https://opensea.io/collection/the-bull-club-og' },
 ];
 
 const DropdownButton = () => {
@@ -37,20 +37,20 @@ const DropdownButton = () => {
 			<button className="button" onClick={toggleDropdown}>
 				<p>MarketPlaces</p>
 				{open ? (
-					<svg style={{ transform: `rotate(180deg)` }} height="20" width="20" viewBox="0 0 20 20" aria-hidden="true" focusable="false" class="css-8mmkcg">
+					<svg style={{ transform: `rotate(180deg)` }} height="20" width="20" viewBox="0 0 20 20" aria-hidden="true" focusable="false" className="css-8mmkcg">
 						<path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z"></path>
 					</svg>
 				) : (
-					<svg height="20" width="20" viewBox="0 0 20 20" aria-hidden="true" focusable="false" class="css-8mmkcg">
+					<svg height="20" width="20" viewBox="0 0 20 20" aria-hidden="true" focusable="false" className="css-8mmkcg">
 						<path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z"></path>
 					</svg>
 				)}
 			</button>
 			{open ? (
 				<ul className="dd-menu">
-					{marketplaces.map(({ label, value, to }, index) => (
+					{marketplaces.map(({ label, value, link }, index) => (
 						<li key={index}>
-							<a className="link " href={to}>
+							<a className="link " href={link} target="_blank" rel="noopener noreferrer">
 								{label}
 							</a>
 						</li>
