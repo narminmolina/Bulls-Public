aecc42a223e009e8389d5814b48295765c0465d8
Narmin Molina <narminmolina@gmail.com>
2022-12-18 10:12:49 -0500

    collection card and modal jsx structure

---
+++ src/components/Collection.modal/index.js
@@ -0,0 +1,26 @@
+const CollectionModal = () => {
+	return (
+		<div className="collection-modal">
+			<div className="bull-info">
+				<div className="bull-profile">
+					<em>CUSTOM</em>
+					<h1>BULL #71</h1>
+					<></>
+				</div>
+				<div className="traits">
+					<h2>Traits</h2>
+					<div className="features"></div>
+				</div>
+			</div>
+			<div className="marketplace-info">
+				<h2>SECONDARY MARKETPLACES</h2>
+				<div className="marketplace-wrapper">
+					<div className="marketplace-box">
+						<span className="logo magic-eden"></span>
+						<button>View on Magic Eden</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
--- src/pages/collection/CollectionCard/index.js
+++ src/pages/collection/CollectionCard/index.js
@@ -3,9 +3,9 @@ import './index.scss';
 const CollectionCard = ({ name, img_url, number }) => {
 	return (
 		<div className="collection-card">
-			<span className="cube">
-				<img src="../../../assets/img/tesseract.webp" alt="tesseract" />
-			</span>
+			<div className="tesseract-box">
+				<span className="teserract-icon"></span>
+			</div>
 			<img src="https://launchpad.webisoft.org/media/minter/solana-bull-3d/V5BbCGK7KKH8oC6mA9EG3f8hH9fxXNRofLA2bNmM9Ss.jpg" alt={name} />
 			<div className="collection-card-caption">
 				<strong>
