9ebd5b8a2abbda50ef083bc3895836c792e3d2ab
Narmin Molina <narminmolina@gmail.com>
2022-12-21 11:46:10 -0500

    transfer state to parent

--- src/pages/collection/Aside/index.js
+++ src/pages/collection/Aside/index.js
@@ -1,77 +1,11 @@
-import { useState, useEffect, useCallback } from 'react';
-import { useDebounce } from 'use-debounce';
-import { isEmpty } from 'lodash';
-import { getTraitTypes, getCollectionItems } from 'api';
-
-import FilterDropdown from 'components/FilterDropdown';
-
 import './index.scss';
 
-const sortByOptions = [
-	{ name: 'ordering', id: 'number', label: 'Bull ID ↑', value: 'number' },
-	{ name: 'ordering', id: '-number', label: 'Bull ID ↓', value: '-number' },
-	{ name: 'ordering', id: 'rarity_score', label: 'Rarity ↑', value: 'rarity_score' },
-	{ name: 'ordering', id: '-rarity_score', label: 'Rarity ↓', value: '-rarity_score' },
-];
-
-const filterByOptions = [
-	{ name: 'bonded', id: 'bonded', label: 'Bonded', value: 1 },
-	{ name: 'bonded', id: 'non-bonded', label: 'Non-bonded', value: 0 },
-];
-
-const Aside = () => {
-	const [traitTypes, setTraitTypes] = useState([]);
-	const [searchResult, setSearchResult] = useState();
-	// const [debouncedSearch] = useDebounce(searchResult, 1000);
-	// const [collection, setCollection] = useState([]);
-
-	// useEffect(() => async () => {
-	// 	const { result } = await getCollectionItems(debouncedSearch);
-	// 	setCollection(result);
-	// });
-
-	const handleSearchInputChange = useCallback(
-		event => {
-			console.log(event.target.value);
-			setSearchResult(result => ({ ...result, search: event.target.value }));
-			console.log(setSearchResult);
-		},
-		[setSearchResult]
-	);
-
-	useEffect(() => {
-		(async () => {
-			const traitTypes = await getTraitTypes();
-			setTraitTypes(traitTypes);
-		})();
-	}, []);
-
+const Aside = ({ title, description, children }) => {
 	return (
 		<aside className="aside">
-			<h1 className="section-title">Collection</h1>
-			<p className="section-description">
-				Search the entire Bull Club Collections.
-				<br /> Or connect your wallet and view your own collection
-			</p>
-			<div className="tab-filter-buttons">
-				<button className="active">Main</button>
-				<button>Owned</button>
-			</div>
-			<div className="search-filters-wrapper">
-				<input type="search" name="search" placeholder="Search" onChange={handleSearchInputChange} />
-				<FilterDropdown title="Sort by" items={sortByOptions} inputType="radio" />
-				<FilterDropdown title="Filter by" items={filterByOptions} inputType="radio" />
-				{traitTypes.map(([key, values]) => (
-					<FilterDropdown key={key} title={key} items={values} />
-				))}
-			</div>
-			<div className="tesseract-key">
-				<strong>KEYS</strong>
-				<div className="key-item">
-					<span className="tesseract-icon"></span>
-					Bonded Tesseract (3d)
-				</div>
-			</div>
+			<h1 className="section-title">{title}</h1>
+			<p className="section-description">{description}</p>
+			{children}
 		</aside>
 	);
 };
--- src/pages/collection/index.js
+++ src/pages/collection/index.js
@@ -1,20 +1,61 @@
 import { useEffect, useCallback, useState } from 'react';
 import { isEmpty, sortBy } from 'lodash';
+import { useDebounce } from 'use-debounce';
 
 import { Button } from 'components/Button';
 import { Base } from 'Base';
 import Aside from './Aside';
+import FilterDropdown from 'components/FilterDropdown';
+
 import CollectionCard from './CollectionCard';
 import './index.scss';
-import { getCollectionItems } from '../../api/index';
+import { getTraitTypes, getCollectionItems } from 'api';
+
+const sortByOptions = [
+	{ name: 'ordering', id: 'number', label: 'Bull ID ↑', value: 'number' },
+	{ name: 'ordering', id: '-number', label: 'Bull ID ↓', value: '-number' },
+	{ name: 'ordering', id: 'rarity_score', label: 'Rarity ↑', value: 'rarity_score' },
+	{ name: 'ordering', id: '-rarity_score', label: 'Rarity ↓', value: '-rarity_score' },
+];
+
+const filterByOptions = [
+	{ name: 'bonded', id: 'bonded', label: 'Bonded', value: 1 },
+	{ name: 'bonded', id: 'non-bonded', label: 'Non-bonded', value: 0 },
+];
 
 const Collection = () => {
 	const [isLoading, setIsLoading] = useState(false);
 	const [collection, setCollection] = useState([]);
+	const [filters, setFilters] = useState({
+		attributes: [],
+		search: '',
+		bonded: null,
+		solana_owner_wallet: null,
+		ordering: 'number',
+	});
 	const [totalCount, setTotalCount] = useState(0);
 	const [nextPageUrl, setNextPageUrl] = useState('');
+	const [traitTypes, setTraitTypes] = useState([]);
+	const [searchResult, setSearchResult] = useState();
+	const [debouncedFilters] = useDebounce(filters, 500);
 
 	console.log({ collection, totalCount, nextPageUrl });
+
+	const handleSearchInputChange = useCallback(
+		event => {
+			console.log(event.target.value);
+			setSearchResult(result => ({ ...result, search: event.target.value }));
+			console.log(setSearchResult);
+		},
+		[setSearchResult]
+	);
+
+	useEffect(() => {
+		(async () => {
+			const traitTypes = await getTraitTypes();
+			setTraitTypes(traitTypes);
+		})();
+	}, []);
 
 	useEffect(() => {
 		(async () => {
@@ -41,7 +82,37 @@ const Collection = () => {
 		<Base title="CollectionPage">
 			<div className="container extended">
 				<div className="collection-wrapper">
-					<Aside />
+					<Aside
+						title="Collection"
+						description={
+							<>
+								Search the entire Bulls and Baby Bulls Collections.
+								<br />
+								Or connect your wallet and view your own collection
+							</>
+						}
+					>
+						<div className="tab-filter-buttons">
+							<button className="active">Main</button>
+							<button>Owned</button>
+						</div>
+						<div className="search-filters-wrapper">
+							<input type="search" name="search" placeholder="Search" onChange={handleSearchInputChange} />
+							<FilterDropdown title="Sort by" items={sortByOptions} setFilters={setFilters} inputType="radio" />
+							<FilterDropdown title="Filter by" items={filterByOptions} setFilters={setFilters} inputType="radio" />
+							{traitTypes.map(([key, values]) => (
+								<FilterDropdown key={key} title={key} items={values} />
+							))}
+						</div>
+						<div className="tesseract-key">
+							<strong>KEYS</strong>
+							<div className="key-item">
+								<span className="tesseract-icon"></span>
+								Bonded Tesseract (3d)
+							</div>
+						</div>
+					</Aside>
+
 					<div className="collection-cards">
 						<div className="card-wrapper">
 							{collection?.map(props => (
